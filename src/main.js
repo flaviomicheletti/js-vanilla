c999042aa0f056bf6ca48a93e2d902bdc872cd94
Flávio Micheletti <flaviomicheletti@hotmail.com>
2020-05-23 13:54:20 -0300

    show data in document

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 // https://jsonplaceholder.typicode.com/
 
 fetch('https://jsonplaceholder.typicode.com/todos')
@@ -6,9 +5,23 @@ fetch('https://jsonplaceholder.typicode.com/todos')
     .then(todos => {
         // console.log(todos);
         todos.forEach(function (item, indice) {
-            console.log(item, indice);
+            //console.log(item, indice);
+            addItem(divMain, createElem(item.title)); 
         });
     })
     .catch(error => {
         console.warn('Failed!', error);
     });
+
+let divMain = document.getElementById('main');
+
+function createElem(text) {
+    let elem  = document.createElement("p");
+    let texto = document.createTextNode(text);
+    elem.appendChild(texto);
+    return elem;
+}
+
+function addItem(elemParent, elemChild) {
+    elemParent.appendChild(elemChild);
+}
