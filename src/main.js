1f2206ed138f6b793f9f2fc083ef0b9fc91a8f00
Flávio Micheletti <flaviomicheletti@hotmail.com>
2020-07-16 13:15:10 -0300

    anexando eventos

--- src/main.js
+++ src/main.js
@@ -10,8 +10,23 @@ fetch('https://jsonplaceholder.typicode.com/todos')
         // console.log(todos);
         todos.forEach(function (item, indice) {
             //console.log(item, indice);
-            addItem(divMain, createElem(item.title)); 
+            addItem(divMain, createElem(item.title));
         });
+
+        //http://www.java2s.com/Tutorials/Javascript/Javascript_Element_How_to/Table_Row/Handle_click_event_for_table_row_tr.htm          
+        // var rows = document.getElementsByTagName("p");
+        // for (var i = 0; i < rows.length; i++) {
+        //     rows[i].onclick = function () {
+        //         console.log(this.textContent);
+        //     };
+        // }
+        // addRowHandlers(divMain);
+        onRowClick(divMain, function (row) {
+            // var value = row.getElementsByTagName("td")[0].innerHTML;
+            // document.getElementById('click-response').innerHTML = value + " clicked!";
+            console.log(row);
+        });
+
     })
     .catch(error => {
         console.warn('Failed!', error);
@@ -20,7 +35,7 @@ fetch('https://jsonplaceholder.typicode.com/todos')
 let divMain = document.getElementById('main');
 
 function createElem(text) {
-    let elem  = document.createElement("p");
+    let elem = document.createElement("p");
     let texto = document.createTextNode(text);
     elem.appendChild(texto);
     return elem;
@@ -29,3 +44,32 @@ function createElem(text) {
 function addItem(elemParent, elemChild) {
     elemParent.appendChild(elemChild);
 }
+
+// https://stackoverflow.com/questions/1207939/adding-an-onclick-event-to-a-table-row
+// function addRowHandlers(parent) {
+//     var rows = parent.getElementsByTagName("p");
+//     for (let i = 0; i < rows.length; i++) {
+//         var currentRow = parent.children[i];
+//         // console.log(parent.children[i]);
+//         var createClickHandler = function (row) {
+//             return function () {
+//                 console.log(Math.random());
+//             };
+//         };
+//         currentRow.onclick = createClickHandler(currentRow);
+//     }
+// }
+
+// https://www.codexpedia.com/javascript/javascript-add-on-click-event-on-table-rows/
+function onRowClick(parent, callback) {
+    var rows = parent.getElementsByTagName("p"),
+        i;
+    for (i = 0; i < rows.length; i++) {
+        parent.children[i].onclick = function (row) {
+            return function () {
+                callback(row);
+            };
+        }(parent.children[i]);
+    }
+};
+
