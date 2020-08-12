ee084e371b754ab34a34f62a85eab0bb259a42da
Flávio Micheletti <flavio.micheletti@midway.com.br>
2020-08-12 13:58:50 -0300

    detalhes

--- src/main.js
+++ src/main.js
@@ -9,27 +9,26 @@
 
 const { handler1, handler2, handler3 } = require("./handlers.js");
 
-fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(response => response.json())
-    .then(todos => {
-        // console.log(todos);
-        todos.forEach(function (item, indice) {
-            // console.log(item.title);
-            addItem(tbody, createElem(item));
-        });
-
-        // handler1(tbody);
-        // handler2(divMain);
-        // handler3(divMain, function (row) {
-        //     console.log(row);
-        // });
-
-    })
-    .catch(error => {
-        console.warn('Failed!', error);
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((response) => response.json())
+  .then((todos) => {
+    // console.log(todos);
+    todos.forEach(function (item, indice) {
+      // console.log(item.title);
+      addItem(tbody, createElem(item));
     });
 
-let main = document.getElementById('main');
+    // handler1(tbody);
+    // handler2(divMain);
+    // handler3(divMain, function (row) {
+    //     console.log(row);
+    // });
+  })
+  .catch((error) => {
+    console.warn("Failed!", error);
+  });
+
+let main = document.getElementById("main");
 
 main.innerHTML = `<table class="table" id="mytable">
 <thead>
@@ -40,21 +39,19 @@ main.innerHTML = `<table class="table" id="mytable">
 </thead>
 <tbody>
 </tbody>
-</table>`
+</table>`;
 
-
-let table = document.getElementById('mytable');
+let table = document.getElementById("mytable");
 let tbody = table.children[1];
 // console.log(table.children[1]);
 
-
 function createElem(item) {
-    let elem = document.createElement("tr");
-    elem.innerHTML = `<th scope="row">${item.id}</th>
+  let elem = document.createElement("tr");
+  elem.innerHTML = `<td scope="row">${item.id}</th>
         <td>${item.title}</td>`;
-    return elem;
+  return elem;
 }
 
 function addItem(elemParent, elemChild) {
-    elemParent.appendChild(elemChild);
+  elemParent.appendChild(elemChild);
 }
