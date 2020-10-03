8540613595d1eb60cb7e041562a00f510caed3b8
Flávio Micheletti <flavio.micheletti@midway.com.br>
2020-10-03 19:10:38 -0300

    form

---
+++ src/form.js
@@ -0,0 +1,16 @@
+const form = `<form>
+<div class="mb-3">
+  <label for="title" class="form-label">Title</label>
+  <input type="text" class="form-control" id="title" />
+</div>
+<div class="mb-3">
+  <label for="userId" class="form-label">User id</label>
+  <input type="text" class="form-control" id="userId" />
+</div>
+<div class="mb-3 form-check">
+  <input type="checkbox" class="form-check-input" id="completed" />
+  <label class="form-check-label" for="completed">Completed</label>
+</div>
+</form>`;
+
+export default form;
